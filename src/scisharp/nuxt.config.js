14222ea1fc3c96c239e59f54f3c0c87ea8f69e60
Meinrad Recheis <meinrad.recheis@gmail.com>
2019-07-14 20:20:14 +0200

    set html languate to en

--- src/scisharp/nuxt.config.js
+++ src/scisharp/nuxt.config.js
@@ -23,14 +23,17 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-,
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
         rel: 'stylesheet',
         href:
           'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
       }
-    ]
+    ],
+      htmlAttrs: {
+          lang: 'en',
+          //amp: true
+      },
   },
   /*
   ** Customize the progress-bar color
