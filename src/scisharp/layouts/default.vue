a0839f9b78868bd310da50a24fed940e3edd0d13
Meinrad Recheis <meinrad.recheis@gmail.com>
2019-07-14 19:35:13 +0200

    initial scisharp page

---
+++ src/scisharp/layouts/default.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-app dark color="violet">
+    <v-toolbar app class="violet elevation-0 theme--dark" >
+      <v-toolbar-title class="headline">
+        <v-layout row>
+          <v-img class="mr-2"
+                 :src="require('../assets/icon.svg')"
+                 contain
+                 style="width: 48px; height: 48px;"
+
+          ></v-img>
+          <div class="my-2 ml-2">
+            <span>SciSharp</span>
+            <span class="font-weight-light text-uppercase"> STACK</span>
+          </div>
+        </v-layout>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+              flat
+              href="https://github.com/SciSharp"
+              target="_blank"
+              class="theme--dark"
+      >
+        <v-layout row>
+
+          <v-img class="mr-2"
+                 :src="require('../assets/github_cat_white.svg')"
+                 contain
+                 style="width: 20px; height: 20px;"
+
+          ></v-img>
+
+          <span class="mr-2">Github</span>
+        </v-layout>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content class="violet">
+      <nuxt />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    components: {
+    },
+    data () {
+      return {
+        //
+      }
+    }
+  }
+</script>
+
+<style>
+  .violet {
+    background-color: #661f76 !important;
+  }
+</style>
---
+++ src/scisharp/pages/index.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-container>
+    <v-layout column
+              text-xs-center
+              wrap
+    >
+      <v-flex xs12>
+        <v-img
+                :src="require('../assets/scisharp.svg')"
+                class="mt-5 mb-3"
+                contain
+                height="300"
+        ></v-img>
+      </v-flex>
+
+      <v-flex mb-5>
+        <h2 class="display-1 font-weight-light mb-3 white--text">
+          A .NET based Open Source ecosystem for Data Science, Machine Learning and AI.
+        </h2>
+      </v-flex>
+
+      <v-flex mb-5 mt-5>
+        <v-layout row>
+          <v-flex xs-3 ma-3>
+          </v-flex>
+          <v-flex xs-3 ma-3>
+            <v-card class="white--text" color="#865fc5">
+              <v-card-title>
+                <h2 class="font-weight-light text-xs-center">Cross-platform with .NET Core</h2>
+              </v-card-title>
+              <v-card-text class="text-xs-left">
+                Lorem ipsum dolor sit amet, brute iriure accusata ne mea. Eos suavitate referrentur ad, te duo agam libris qualisque, utroque quaestio accommodare no qui. Et percipit laboramus usu, no invidunt verterem nominati mel. Dolorem ancillae an mei, ut putant invenire splendide mel, ea nec propriae adipisci. Ignota salutandi accusamus in sed, et per malis fuisset, qui id ludus appareat.
+              </v-card-text>
+            </v-card>
+          </v-flex>
+
+          <v-flex xs-3 ma-3>
+            <v-card class="white--text" color="#865fc5">
+              <v-card-title>
+                <h2 class="font-weight-light text-xs-center">Huge ML/AI Knowledge Base</h2>
+              </v-card-title>
+              <v-card-text class="text-xs-left">
+                Lorem ipsum dolor sit amet, brute iriure accusata ne mea. Eos suavitate referrentur ad, te duo agam libris qualisque, utroque quaestio accommodare no qui. Et percipit laboramus usu, no invidunt verterem nominati mel. Dolorem ancillae an mei, ut putant invenire splendide mel, ea nec propriae adipisci. Ignota salutandi accusamus in sed, et per malis fuisset, qui id ludus appareat.
+              </v-card-text>
+            </v-card>
+          </v-flex>
+
+          <v-flex xs-3 ma-3>
+            <v-card class="white--text" color="#865fc5">
+              <v-card-title>
+                <h2 class="font-weight-light text-xs-center">Open Source</h2>
+              </v-card-title>
+              <v-card-text class="text-xs-left">
+                Lorem ipsum dolor sit amet, brute iriure accusata ne mea. Eos suavitate referrentur ad, te duo agam libris qualisque, utroque quaestio accommodare no qui. Et percipit laboramus usu, no invidunt verterem nominati mel. Dolorem ancillae an mei, ut putant invenire splendide mel, ea nec propriae adipisci. Ignota salutandi accusamus in sed, et per malis fuisset, qui id ludus appareat.
+              </v-card-text>
+            </v-card>
+          </v-flex>
+          <v-flex xs-3 ma-3>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+    })
+  }
+</script>
+
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    // Logo,
+    // VuetifyLogo
+  }
+}
+</script>
