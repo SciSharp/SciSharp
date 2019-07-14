6f5eb7c6c6782ed9c7043039a917e773dab415a5
Meinrad Recheis <meinrad.recheis@gmail.com>
2019-07-14 20:23:15 +0200

    favicon fix

--- src/scisharp/nuxt.config.js
+++ src/scisharp/nuxt.config.js
@@ -23,7 +23,7 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/SciSharp/favicon.ico' },
       {
         rel: 'stylesheet',
         href:
